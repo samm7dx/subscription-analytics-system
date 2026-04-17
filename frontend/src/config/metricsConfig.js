612f6e65df4d1f5ca186c6bcbfd3ca317df83ac6
SAMRIDH RAJ <samridh732@gmail.com>
2026-04-17 16:58:49 +0530

    Create metricsConfig.js

---
+++ frontend/src/config/metricsConfig.js
@@ -0,0 +1,10 @@
+export const metrics = [
+  {
+    id: "dau",
+    title: "Daily Active Users",
+    description: "Unique users active per day",
+    why: "Measures engagement trends",
+    api: "/api/run-query/dau",
+    chart: "line",
+  }
+];
