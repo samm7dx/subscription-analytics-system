5d838759a878792dfebed4324869133cd4da37c0
SAMRIDH RAJ <samridh732@gmail.com>
2026-04-17 17:01:07 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,10 @@
-
+export const runQuery = async (endpoint) => {
+  try {
+    const res = await fetch(`http://localhost:5000${endpoint}`);
+    if (!res.ok) throw new Error("API error");
+    return await res.json();
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
