ee7df14eca06a1c28f5586e850d9fc2ff7233958
SAMRIDH RAJ <samridh732@gmail.com>
2026-04-17 17:06:10 +0530

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,1 +1,8 @@
-analyticsRoutes.js
+import express from "express";
+import { runQuery } from "../controllers/analyticsController.js";
+
+const router = express.Router();
+
+router.get("/run-query/:type", runQuery);
+
+export default router;
