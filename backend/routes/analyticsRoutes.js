dd2194493607059ba50b41047a0f8725fbfaf7d1
SAMRIDH RAJ <samridh732@gmail.com>
2026-04-17 16:52:55 +0530

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,1 +1,7 @@
-analyticsRoutes.js
+const express = require("express");
+const router = express.Router();
+const { runQuery } = require("../controllers/analyticsController");
+
+router.get("/run-query/:type", runQuery);
+
+module.exports = router;
