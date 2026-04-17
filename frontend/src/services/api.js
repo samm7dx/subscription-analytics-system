45793fb77993e3d7cd22f6ee80382f3cb24676e4
SAMRIDH RAJ <samridh732@gmail.com>
2026-04-17 17:10:24 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,11 @@
 export const runQuery = async (endpoint) => {
   try {
-    const res = await fetch(`http://localhost:5000${endpoint}`);
+   const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const runQuery = async (endpoint) => {
+  const res = await fetch(`${BASE_URL}${endpoint}`);
+  return res.json();
+};
     if (!res.ok) throw new Error("API error");
     return await res.json();
   } catch (e) {
